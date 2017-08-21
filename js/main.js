ff33779e511a4f3bdcc8c47663271191d99ddfce
Antonia Elsen <antoniaelsen@gmail.com>
2017-08-21 04:31:15 -0700

    Add image uploading functionality

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
 var imageLoaded = false;
 
+var canvas_wrapper = document.getElementById('canvas-wrapper');
+var canvas = document.getElementById('editor');
+var ctx = canvas.getContext('2d');
+
+var canvas_img = new Image();
+
 document.getElementById("save-button").disabled = !imageLoaded;
 document.getElementById("clear-button").disabled = !imageLoaded;
 
-document.getElementById("load-button").addEventListener("click", handleImageLoad);
-document.getElementById("clear-button").addEventListener("click", handleImageClear);
+document.getElementById("load-button").addEventListener("click", handleButtonLoad);
+document.getElementById("clear-button").addEventListener("click", handleButtonClear);
+document.getElementById("image-input").addEventListener("change", handleImageInput);
 
-function handleImageLoad() {
-    imageLoaded = true;
-    toggleImageButtons(!imageLoaded);
+function handleButtonLoad() {
+    document.getElementById('image-input').click();
 }
 
-function handleImageClear() {
+function handleButtonClear() {
     imageLoaded = false;
     toggleImageButtons(!imageLoaded);
 }
@@ -21,3 +27,66 @@ function toggleImageButtons(state){
   document.getElementById("save-button").disabled = state;
   document.getElementById("clear-button").disabled = state;
 }
+
+function handleImageInput(e){
+  var reader = new FileReader();
+
+  reader.readAsDataURL(e.target.files[0]);
+  reader.onload = function(event){
+    var img = new Image();
+    img.src = event.target.result;
+
+    img.onload = function(){
+      canvas_img = img;
+      redrawCanvas();
+    }
+
+    imageLoaded = true;
+    toggleImageButtons(!imageLoaded)
+
+  }
+}
+
+function redrawCanvas(){
+  console.log("redrawing canvas");
+  var img = canvas_img;
+  var cw = canvas_wrapper.offsetWidth;
+  var ch = canvas_wrapper.offsetHeight;
+
+  console.log("s width: " + img.width +
+    " / s height: " + img.height);
+
+  var scaling_factor =
+    scaleWithAspect(img.width, img.height, cw, ch);
+
+  console.log("d width: " + img.width * scaling_factor +
+    " / d height: " + img.height * scaling_factor);
+
+  resizeCanvas(img.width * scaling_factor, img.height * scaling_factor);
+  ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0,
+    img.width * scaling_factor, img.height * scaling_factor);
+}
+
+function resizeCanvas(width, height){
+  canvas.width = width;
+  canvas.height = height;
+}
+
+function scaleWithAspect(src_width, src_height, dest_width, dest_height){
+    var scale_factor = 1;
+    scale_factor = Math.min((dest_width / src_height), (dest_height / src_height));
+    console.log("Scale factor:", scale_factor);
+    return scale_factor;
+}
+
+var addEvent = function(object, type, callback) {
+    if (object == null || typeof(object) == 'undefined') return;
+    if (object.addEventListener) {
+        object.addEventListener(type, callback, false);
+    } else if (object.attachEvent) {
+        object.attachEvent("on" + type, callback);
+    } else {
+        object["on"+type] = callback;
+    }
+};
+addEvent(window, "resize", redrawCanvas);
